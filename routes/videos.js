1256dab1210d7219dac9eb67fb1f723b9d8bebbe
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-09 20:39:18 -0500

    server now sends correct video data to client side

--- routes/videos.js
+++ routes/videos.js
@@ -1,20 +1,41 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+
+const getVideos = () => {
+  const videos = fs.readFileSync("./data/videos.json");
+  return JSON.parse(videos);
+};
 
 router.get("/", (req, res) => {
-  res.send("test");
+  let formattedVideos = getVideos().map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+    };
+  });
+  res.status(200).json(formattedVideos);
 });
 
 router.get("/:id", (req, res) => {
-  res.send("test id");
+  const individualVideo = getVideos().find(video => video.id === req.params.id);
+
+    if (!individualVideo) {
+        res.status(404).json({
+            message: "Team not found"
+        })
+        return;
+    }
+    
+    res.status(200).json(individualVideo)
 });
 
-router.put("/:id", (req, res) => {
-  res.send("test id");
-});
+
 
 router.delete("/:id", (req, res) => {
   res.send("test id");
 });
 
-module.exports = router
+module.exports = router;
