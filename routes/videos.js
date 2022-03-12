0f2632a8873f50e8592f0a73a3c593b0ad673293
Philip Aguiar <PhilipAguiar@gmail.com>
2022-03-11 19:45:14 -0500

    created post and delete requests on the server side

--- routes/videos.js
+++ routes/videos.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+router.use(express.json());
 
 const getVideos = () => {
   const videos = fs.readFileSync("./data/videos.json");
   return JSON.parse(videos);
+};
+
+const saveVideos = (videos) => {
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videos));
 };
 
 router.get("/", (req, res) => {
@@ -20,22 +27,58 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const individualVideo = getVideos().find(video => video.id === req.params.id);
+  const individualVideo = getVideos().find((video) => video.id === req.params.id);
 
-    if (!individualVideo) {
-        res.status(404).json({
-            message: "Team not found"
-        })
-        return;
-    }
-    
-    res.status(200).json(individualVideo)
+  if (!individualVideo) {
+    res.status(404).json({
+      message: "Team not found",
+    });
+    return;
+  }
+
+  res.status(200).json(individualVideo);
 });
 
+router.post("/:id/comments", (req, res) => {
+  let videos = getVideos();
+  const currentDate = new Date();
 
+  let newVideo = videos.find((video) => video.id === req.params.id);
+  // id: uuidv4(),
+    newVideo.comments.push({
+    id: uuidv4(),
+    name: req.body.name,
+    comment: req.body.comment,
+    likes: 0,
+    timestamp: currentDate.getTime(),
+  });
 
-router.delete("/:id", (req, res) => {
-  res.send("test id");
+  let newVideoList = (videos.map((video)=>{
+    if(video.id === req.params.id){
+      
+      return newVideo
+    }
+  
+    return video
+  }))
+  saveVideos(newVideoList);
+  res.status(200).json(newVideoList);
+});
+
+router.delete("/:id/comments/:commentId", (req, res) => {
+  let videos = getVideos();
+  let newVideos = videos.map((video) => {
+    let newComments = video.comments.filter((comment) => {
+      if (comment.id !== req.params.commentId) {
+        return comment;
+      }
+    });
+    video.comments = newComments;
+    return video;
+  });
+  console.log(newVideos);
+  saveVideos(newVideos);
+  res.status(200).json(newVideos);
 });
 
 module.exports = router;
